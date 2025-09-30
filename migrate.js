312ffcceb830d99882d1a7d54d999e3f86c321be
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-09-30 15:31:16 +0700

    chore: add force option to migrate command
    Adds a force option to the migrate command to allow for
    recreating the database.

--- migrate.js
+++ migrate.js
@@ -3,16 +3,21 @@ import { UsersModel } from "./src/models/UsersModel.js";
 import { RefreshTokenModel } from "./src/models/RefreshTokenModel.js";
 import { LinksModel } from "./src/models/LinksModel.js";
 
+const args = process.argv[2] || "";
+const isForce = args.includes("force");
+
 async function migrate() {
   try {
     await DB.authenticate();
-    console.log("DBconnected");
+    console.log("DBconnected, continue to sync with force = " + isForce);
     await DB.sync({
-      force: true,
+      force: isForce,
     });
-    console.log("migrate done");
+    console.log("sync done");
   } catch (error) {
     console.log(error);
+  } finally {
+    await DB.close();
   }
 }
 
