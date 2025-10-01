fcacaeaf0f801a4c8d15d5dad33960aea0a550bd
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-10-02 04:16:54 +0700

    feat: set middleware to decoded refreshtoken

---
+++ src/middleware/refreshTokenCookieMiddleware.js
@@ -0,0 +1,41 @@
+import { Op } from "sequelize";
+import { RefreshTokenModel } from "../models/RefreshTokenModel.js";
+import { AuthError } from "../utils/error.js";
+import { verifyToken } from "../utils/token.js";
+
+export async function refreshTokenCookieMiddleware(request, response, next) {
+  try {
+    const refresh_token = request.cookies?.["refresh_token"];
+    if (!refresh_token) {
+      throw new AuthError("invalid credentials");
+    }
+
+    const decodedUser = await verifyToken(refresh_token, true);
+
+    if (!decodedUser) {
+      throw new AuthError("invalid credentials");
+    }
+
+    console.log(decodedUser);
+
+    const refreshTokenInDB = await RefreshTokenModel.findOne({
+      where: {
+        token: refresh_token,
+        userId: decodedUser.id,
+        isRevoked: false,
+        expiredAt: {
+          [Op.gt]: new Date(),
+        },
+      },
+    });
+
+    if (!refreshTokenInDB) {
+      throw new AuthError("invalid credentials");
+    }
+
+    request.refreshtokenId = refreshTokenInDB.id;
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
