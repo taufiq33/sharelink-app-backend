d935bbe31e5c04c91c30cffd56d28c8e5b118b8a
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-10-10 20:32:49 +0700

    feat(migrate script): add alter support

--- migrate.js
+++ migrate.js
@@ -5,18 +5,31 @@ import { LinksModel } from "./src/models/LinksModel.js";
 
 const args = process.argv[2] || "";
 const isForce = args.includes("force");
+const isAlter = args.includes("alter");
 
 async function migrate() {
   try {
     await DB.authenticate();
-    console.log("DBconnected, continue to sync with force = " + isForce);
-    await DB.sync({
-      force: isForce,
-    });
-    console.log("sync done");
+    if (isForce) {
+      console.log("DBconnected, continue to sync with force = " + isForce);
+      await DB.sync({
+        force: isForce,
+      });
+    } else if (isAlter) {
+      console.log("DBconnected, continue to sync with alter = " + isAlter);
+      await DB.sync({
+        alter: true,
+      });
+    } else {
+      console.log("DBconnected, continue to sync with force = " + isForce);
+      await DB.sync({
+        force: false,
+      });
+    }
   } catch (error) {
     console.log(error);
   } finally {
+    console.log("sync done");
     await DB.close();
   }
 }
