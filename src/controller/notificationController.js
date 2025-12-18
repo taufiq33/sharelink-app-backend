f82f7b53e0a65f0b6b6e5105bd5e571365d1a1c9
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-12-18 11:22:53 +0700

    feat(notification): add mark all as read and sort user notifications

--- src/controller/notificationController.js
+++ src/controller/notificationController.js
@@ -55,6 +55,34 @@ export async function markNotificationAsRead(request, response, next) {
   }
 }
 
+export async function markAllNotificationAsRead(request, response, next) {
+  try {
+    const [updateAsRead] = await NotificationsModel.update(
+      {
+        isRead: true,
+      },
+      {
+        where: {
+          userId: request.user.id,
+        },
+      },
+    );
+
+    if (updateAsRead === 0) {
+      throw Error("internal DB error");
+    }
+
+    response.json({
+      success: true,
+      data: {
+        message: "mark all notification as read done",
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
 export async function fetchNotifications(request, response, next) {
   try {
     const notifications = await NotificationsModel.findAll({});
@@ -77,6 +105,7 @@ export async function fetchNotificationByUser(request, response, next) {
       where: {
         userId: request.user.id,
       },
+      order: [["createdAt", "DESC"]],
     });
 
     response.json({
--- src/router/notificationRouter.js
+++ src/router/notificationRouter.js
@@ -3,11 +3,13 @@ import { authMiddleware } from "../middleware/authMiddleware.js";
 import {
   fetchNotificationByUser,
   markNotificationAsRead,
+  markAllNotificationAsRead,
 } from "../controller/notificationController.js";
 
 const router = e.Router();
 
 router.patch("/read/:id", authMiddleware, markNotificationAsRead);
+router.put("/readAll/", authMiddleware, markAllNotificationAsRead);
 router.post("/", authMiddleware, fetchNotificationByUser);
 
 export const notificationRouter = router;
